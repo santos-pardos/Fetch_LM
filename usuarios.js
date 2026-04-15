156eddc059d6fead7222551500cc5d75607f8a3b
profesantos <66181446+santos-pardos@users.noreply.github.com>
2026-04-15 08:16:19 +0200

    Delete usuarios.js

--- usuarios.js
+++
@@ -1,6 +0,0 @@
-[
-    { "id": "silla", "nombre": "Silla", "precio": 1500 },
-    { "id": "sillon", "nombre": "Sillón", "precio": 2500 },
-    { "id": "puerta", "nombre": "Puerta", "precio": 3500 },
-    { "id": "ventana", "nombre": "Ventana", "precio": 4500 }
-]
